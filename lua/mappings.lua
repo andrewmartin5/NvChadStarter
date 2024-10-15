2617841d63e643b19b69ce9bd479fb04116a2ea1
andrewmartin5 <andrewmlefty5@gmail.com>
2024-10-14 22:11:07 -0400

    Ctrl+backspace deletes word

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("i", "<C-BS>", "<C-W>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
